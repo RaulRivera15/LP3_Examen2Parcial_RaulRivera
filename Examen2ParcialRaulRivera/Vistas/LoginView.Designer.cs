fcdba3eec128133446c8db831cba76aeadcf6c97
RaulRivera15 <90355853+RaulRivera15@users.noreply.github.com>
2021-11-12 09:36:54 -0600

    Creación de Proyecto y Login
    Creación del proyecto y la conexión a la base de datos

---
+++ Examen2ParcialRaulRivera/Vistas/LoginView.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Examen2ParcialRaulRivera.Vistas
+{
+    partial class LoginView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.ClaveTextBox = new System.Windows.Forms.TextBox();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.Location = new System.Drawing.Point(347, 196);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(100, 38);
+            this.CancelarButton.TabIndex = 11;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.Location = new System.Drawing.Point(182, 196);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(94, 38);
+            this.AceptarButton.TabIndex = 10;
+            this.AceptarButton.Text = "Aceptar";
+            this.AceptarButton.UseVisualStyleBackColor = true;
+            // 
+            // ClaveTextBox
+            // 
+            this.ClaveTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClaveTextBox.Location = new System.Drawing.Point(221, 128);
+            this.ClaveTextBox.Name = "ClaveTextBox";
+            this.ClaveTextBox.PasswordChar = '*';
+            this.ClaveTextBox.Size = new System.Drawing.Size(293, 29);
+            this.ClaveTextBox.TabIndex = 9;
+            this.ClaveTextBox.Text = "1234";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailTextBox.Location = new System.Drawing.Point(221, 63);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(293, 29);
+            this.EmailTextBox.TabIndex = 8;
+            this.EmailTextBox.Text = "raulrivera@gmail.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Contraseña:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Email:";
+            // 
+            // LoginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 296);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.ClaveTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginView";
+            this.Text = "Inicio de Sesión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button CancelarButton;
+        public System.Windows.Forms.Button AceptarButton;
+        public System.Windows.Forms.TextBox ClaveTextBox;
+        public System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
